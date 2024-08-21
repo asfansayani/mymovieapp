3fab7b18d7de0bee31ae738810e4fc9db2a5e8af
Asfan Sayani <asfan.sayani@invinci.pk>
2024-08-21 07:48:08 -0700

    config changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
         <>
             <Navbar>
                 <Container>
-                    <Navbar.Brand href="#home"><b>MY MOVIE APP</b></Navbar.Brand>
+                    <Navbar.Brand href="/mymovieapp"><b>MY MOVIE APP</b></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto align-items-center">
-                            <NavLink className={(e) => e.isActive ? "nav-link active" : "nav-link text-white"} to="/">Home</NavLink>
+                            <NavLink className={(e) => e.isActive ? "nav-link active" : "nav-link text-white"} to="/mymovieapp">Home</NavLink>
                             <NavLink className={(e) => e.isActive ? "nav-link active" : "nav-link text-white"} to="/blog">Blog</NavLink>
                             <NavLink className={(e) => e.isActive ? "nav-link active" : "nav-link text-white"} to="/movies">Movies</NavLink>
                         </Nav>
